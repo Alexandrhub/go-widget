7519f058ca94a8e8149f893ce18fcddfc9b71fdb
Alexandr <alexandrhub@vk.com>
2024-02-04 18:17:54 +0300

    feat: implimented base btc widget

--- main.go
+++ main.go
@@ -1,15 +1,55 @@
 package main
 
-import "github.com/getlantern/systray"
+import (
+	"net/http"
+
+	"github.com/PuerkitoBio/goquery"
+	"github.com/getlantern/systray"
+	"github.com/robfig/cron/v3"
+)
+
+type state struct {
+	Price string
+	Cron  *cron.Cron
+}
 
 func main() {
-	systray.Run(onReady, onExit)
+	s := &state{}
+	systray.Run(s.onReady, s.onExit)
 }
 
-func onReady() {
-	systray.SetTitle("Systray example")
-	systray.SetTooltip("Systray example")
+func (s *state) onReady() {
+	s.updatePrice()
+	s.Cron = cron.New()
+	s.Cron.AddFunc("@every 1m", func() { s.updatePrice() })
+	s.Cron.Start()
 }
 
-func onExit() {
+func (s *state) onExit() {
+	s.Cron.Stop()
 }
+
+func (s *state) updatePrice() {
+	url := "https://coinmarketcap.com/currencies/bitcoin/"
+	resp, err := http.Get(url)
+	if err != nil {
+		return
+	}
+	defer resp.Body.Close()
+
+	if resp.StatusCode != http.StatusOK {
+		return
+	}
+
+	doc, err := goquery.NewDocumentFromReader(resp.Body)
+	if err != nil {
+		return
+	}
+
+	price := doc.Find(".sc-f70bb44c-0.jxpCgO.base-text").Text()
+
+	systray.SetTitle("BTC $: " + price)
+}
+
+// https://coinmarketcap.com/currencies/bitcoin/
+//<span class="sc-f70bb44c-0 jxpCgO base-text">$42,851.89</span>
